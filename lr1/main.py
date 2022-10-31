0ab1cefa4f024d421e1202aa21cb0a257bb26538
maxim2220 <78800220+maxim2220@users.noreply.github.com>
2022-10-31 10:12:24 +0300

    1 preload

--- lr1/main.py
+++ lr1/main.py
@@ -4,22 +4,34 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def get_func(tag):
+    tag1 = f"<{tag}>"
+    tag2 = f"</{tag}>"
+    print("<ol>")
+
+
+    def func(string):
+        nonlocal tag1
+        nonlocal tag2
+        for i in range(len(string)):
+            print(f"{tag1}  {string[i]}  {tag2}")
+        print("</ol>")
+
+    return func
+
+
 # Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
+    a = ['Список 1', 'Список 2', '...', 'Список N']
+    new_tag = get_func("li")
+    new_tag(a)
 
-    def get_func(tag):
-        tag1 = "<" + tag + ">"
-        tag2 = "</" + tag + ">"
-        print("<ol>")
-        def func(string):
-            nonlocal tag1
-            nonlocal tag2
-            for i in range(len(string)):
-                print(tag1 + " " + string[i] + " " + tag2)
-            print("</ol>")
-        return func
-
-a = input().split()
-new_tag = get_func("li")
-new_tag(a)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
